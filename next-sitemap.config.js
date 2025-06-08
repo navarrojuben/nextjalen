898c57cee7df7455f9c5b611a3dab28da9431952
Juben Navarro <juben.lnavarro@gmail.com>
2025-06-08 09:43:22 +0800

    next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nextjalen.vercel.app',
+  generateRobotsTxt: true, // (optional) generate robots.txt
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  // You can also add exclude, additionalPaths, etc.
+};
